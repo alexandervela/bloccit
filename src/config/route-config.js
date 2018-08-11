2e42727adfa62b561b4cb12611dc5a1f4e84f126
Alexander Vela <alexander@Alexanders-MacBook-Pro.local>
2018-08-10 22:50:05 -0400

    checkpoint 11 complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,11 +3,13 @@ module.exports = {
       const staticRoutes = require("../routes/static");
       const postRoutes = require("../routes/posts");
       const flairRoutes = require("../routes/flairs");
+      const userRoutes = require("../routes/users");
       const topicRoutes = require("../routes/topics");
       const adRoutes = require("../routes/advertisements");
       app.use(staticRoutes);
       app.use(postRoutes);
       app.use(flairRoutes);
+      app.use(userRoutes);
       app.use(topicRoutes);
       app.use(adRoutes);
     }
